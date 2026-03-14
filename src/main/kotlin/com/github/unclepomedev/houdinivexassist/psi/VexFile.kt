4f9729033e254c4739a415912d915b2298d02b21
Pome <unclepome.dev@gmail.com>
2026-03-14 10:31:32 +0900

    Reorganize VEX language and PSI classes into subpackages
    Move language-related components to the `.lang` package and PSI-related classes to the `.psi` package to improve project structure. Package declarations and imports have been updated throughout the codebase and in plugin.xml.

---
+++ src/main/kotlin/com/github/unclepomedev/houdinivexassist/psi/VexFile.kt
@@ -0,0 +1,12 @@
+package com.github.unclepomedev.houdinivexassist.psi
+
+import com.github.unclepomedev.houdinivexassist.lang.VexFileType
+import com.github.unclepomedev.houdinivexassist.lang.VexLanguage
+import com.intellij.extapi.psi.PsiFileBase
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.psi.FileViewProvider
+
+class VexFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, VexLanguage.Companion.INSTANCE) {
+    override fun getFileType(): FileType = VexFileType
+    override fun toString(): String = "VEX File"
+}
