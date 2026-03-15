adf9d53875ca2387cd3b25a6a650cde06a8bf09f
Pome <unclepome.dev@gmail.com>
2026-03-15 09:30:19 +0900

    Add unit tests for function completion and API provider
    Verify that the completion contributor suggests standard VEX functions and ensure the API provider correctly loads signatures and return types from the data source.

---
+++ src/test/kotlin/com/github/unclepomedev/houdinivexassist/services/VexApiProviderTest.kt
@@ -0,0 +1,25 @@
+package com.github.unclepomedev.houdinivexassist.services
+
+import com.github.unclepomedev.houdinivexassist.VexTestBase
+
+class VexApiProviderTest : VexTestBase() {
+
+    fun testApiProviderLoadsFunctions() {
+        val provider = project.getService(VexApiProvider::class.java)
+        val functions = provider.functions
+
+        assertNotNull("Functions list should not be null", functions)
+        assertTrue(
+            "Functions list should not be empty. Make sure vex_api_dump.json is generated.",
+            functions.isNotEmpty()
+        )
+
+        val distanceFunc = functions.find { it.name == "distance" }
+        assertNotNull("Should contain 'distance' function", distanceFunc)
+
+        if (distanceFunc != null) {
+            assertTrue("distance function should have arguments", distanceFunc.args.isNotEmpty())
+            assertTrue("distance function should have a return type", distanceFunc.returnType.isNotEmpty())
+        }
+    }
+}
