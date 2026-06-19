0f419b24aca052110463b9575ee3617dba1fed7a
Pome <unclepome.dev@gmail.com>
2026-06-20 03:39:16 +0900

    Improve "Ne File" action availability and menu organization

--- src/main/kotlin/com/github/unclepomedev/houdinivexassist/actions/CreateVexFileAction.kt
+++ src/main/kotlin/com/github/unclepomedev/houdinivexassist/actions/CreateVexFileAction.kt
@@ -3,6 +3,7 @@ package com.github.unclepomedev.houdinivexassist.actions
 import com.github.unclepomedev.houdinivexassist.icons.VexIcons
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 
@@ -10,7 +11,7 @@ class CreateVexFileAction : CreateFileFromTemplateAction(
     ACTION_TITLE,
     ACTION_DESCRIPTION,
     VexIcons.FILE
-) {
+), DumbAware {
     companion object {
         private const val ACTION_TITLE = "VEX File"
         private const val ACTION_DESCRIPTION = "Creates a new VEX file"
